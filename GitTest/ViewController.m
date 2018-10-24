434d9f4897a7d30133dba2d493fadb981732c31b
Apple <anzhenli@anzhenlideimac.local>
2018-10-24 09:31:29 +0800

    修改了ViewController

--- GitTest/ViewController.m
+++ GitTest/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (assign, nonatomic) BOOL isReplace;
 @end
 
 @implementation ViewController
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor redColor];
 }
 
 
